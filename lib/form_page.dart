8c91de22806387d8a2bcb0ccfbe0a5c84c204274
isro24 <alamat_email_anda@example.com>
2025-03-19 19:52:31 +0700

    Menambah IconButton calender untuk DateTimePicker

--- lib/form_page.dart
+++ lib/form_page.dart
@@ -15,6 +15,34 @@ class _FormPageState extends State<FormPage> {
         title: Text('Form Page'),
         centerTitle: true,
       ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("Task Date:"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        
+                      ],
+                    ),
+                    IconButton(
+                      onPressed: (){},
+                      icon: const Icon(Icons.calendar_today, color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
